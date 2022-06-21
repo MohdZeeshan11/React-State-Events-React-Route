98160195aaa67dedd7583307452c624f469e95f6
Mohd Zeeshan <100269606+MohdZeeshan11@users.noreply.github.com>
2022-06-21 10:35:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
+
 const App = () => {
-  
+  const[state,setState] = useState('Marco');  
+  const[changeVal,setChangeVal] = useState(false);
+  const[val,setVal] = useState('Polo');
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{state}</h1>
+      <button id="marco-polo-toggler" onClick={function(){
+        setChangeVal(!changeVal);
+        if(!changeVal){
+          setState('Polo')
+          setVal('Marco')
+        }else{
+          setState('Marco')
+          setVal('Polo')
+        }
+      }}>{val}</button>
     </div>
   )
 }
